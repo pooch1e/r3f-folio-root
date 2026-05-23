abbf9ab9b7b514253ad6850c6b2b370fb264a98f
Joel Kram <joel.kram@believe.com>
2026-05-23 21:03:28 +0100

    add folio iframe - needs positioning

--- app/Experience.jsx
+++ app/Experience.jsx
@@ -1,32 +1,25 @@
-import { OrbitControls } from '@react-three/drei'
-import { useThree } from '@react-three/fiber'
-import { useLoader } from '@react-three/fiber'
-import { GLTFLoader } from 'three-stdlib'
+import { useGLTF } from '@react-three/drei'
 import { Environment } from '@react-three/drei'
 import { PresentationControls, ContactShadows, Html } from '@react-three/drei'
 
 export default function Experience() {
-  const computerModel = useLoader(GLTFLoader, '/models/computer/zombie_computer.glb')
+  const { scene, nodes } = useGLTF('/models/computer/zombie_computer.glb')
+
   return (
     <>
       <color args={['#241a1a']} attach='background' />
       <Environment preset='city' />
-      // Computer model
-      <PresentationControls global rotation={[0.13, 0.1, 0]} polar={[-0.4, 0.2]} azimuth={[-0.8, 0.3]}>
-        <primitive
-          object={computerModel.scene}
-          scale={0.5}
-          position-y={-1.5}
-          position-z={-1.5}
-          position-x={1.5}
-          rotation-y={1}
-        />
+
+      <PresentationControls global rotation={[0.2, 0.1, 0]} polar={[-0.4, 0.2]} azimuth={[-0.8, 0.3]}>
+        <primitive object={scene} scale={0.5} position={[1.0, -1.5, -2.5]} rotation-y={1}></primitive>
         <Html transform wrapperClass='htmlScreen' distanceFactor={1.17}>
-          This will be my site iframe
-          <iframe src='https://google.com/html' />
+          <iframe src='https://joelkram.dev' />
         </Html>
       </PresentationControls>
+
       <ContactShadows position-y={-1.3} opacity={0.4} scale={5} blur={2.4} />
     </>
   )
 }
+
+useGLTF.preload('/models/computer/zombie_computer.glb')

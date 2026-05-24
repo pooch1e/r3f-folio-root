2e91f561081398d1b3e9829be94d2fffa8c92b4a
Joel Kram <joel.kram@believe.com>
2026-05-24 22:55:38 +0100

    add video texture to screen node

--- app/Experience.jsx
+++ app/Experience.jsx
@@ -1,22 +1,37 @@
 import { useGLTF } from '@react-three/drei'
 import { Environment } from '@react-three/drei'
 import { PresentationControls, ContactShadows, Html } from '@react-three/drei'
+import { useVideoTexture } from '@react-three/drei'
+import { useEffect } from 'react'
+import { RepeatWrapping } from 'three'
 
 export default function Experience() {
   const { scene, nodes } = useGLTF('/models/computer/computer_nodeNamed.glb')
   console.log(nodes)
   // replace 'screen' node with video texture
+  const videoTexture = useVideoTexture('/videos/dev_compressed.mp4')
 
+  useEffect(() => {
+    videoTexture.wrapS = RepeatWrapping
+    videoTexture.wrapT = RepeatWrapping
+    videoTexture.offset.set(-0.08, 0.01)
+
+    videoTexture.flipY = true
+    videoTexture.rotation = Math.PI /2
+    scene.traverse((child) => {
+      if (child.isMesh && child.parent?.name === 'screen') {
+        child.material.map = videoTexture
+        child.material.needsUpdate = true
+      }
+    })
+  }, [scene, videoTexture])
   return (
     <>
       <color args={['#241a1a']} attach='background' />
       <Environment preset='city' />
 
       <PresentationControls global rotation={[0.2, 0.1, 0]} polar={[-0.4, 0.2]} azimuth={[-0.8, 0.3]}>
-        <primitive object={scene} scale={0.5} position={[1.0, -1.5, -2.5]} rotation-y={1}></primitive>
-        <Html transform wrapperClass='htmlScreen' distanceFactor={1.17}>
-          <iframe src='https://joelkram.dev' />
-        </Html>
+        <primitive object={scene} scale={0.5} position={[2.0, -1.5, -2.5]} rotation-y={1}></primitive>
       </PresentationControls>
 
       <ContactShadows position-y={-1.3} opacity={0.4} scale={5} blur={2.4} />
